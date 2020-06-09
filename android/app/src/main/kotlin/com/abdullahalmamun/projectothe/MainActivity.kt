0f2d61da46794f1a6307d893505e89ebc3bd8eca
kabbo.nibir <kabbo.nibir@gmail.com>
2020-06-09 20:05:39 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/abdullahalmamun/projectothe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.abdullahalmamun.projectothe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('App Title'),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: RefactorCard(
+                    color: Color(0xFF1D1E33),
+                  ),
+                ),
+                //SizedBox(width: 5.0),
+                Expanded(
+                  child: RefactorCard(
+                    color: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: RefactorCard(
+              color: Color(0xFF1D1E33),
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: RefactorCard(
+                    color: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: RefactorCard(
+                    color: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Color(0xFFEB1555),
+            width: double.infinity,
+            height: 90,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class RefactorCard extends StatelessWidget {
+  final Color color;
+  RefactorCard({@required this.color});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: color,
+        /*border: Border.all(
+          color: Colors.white,
+        ),*/
+      ),
+    );
+  }
+}
